f48b9de93b72de381eef3c173bbf9f598fd854f6
Evgeni Rovneiko <erovneiko@mail.ru>
2020-06-30 16:28:40 +0300

    исправил счётчик в 04_vocal.rb

--- lesson_03/04_vocal.rb
+++ lesson_03/04_vocal.rb
@@ -3,7 +3,7 @@ hash = Hash.new
 alphabet = ('а'..'я').to_a
 
 alphabet.each_index do |i| 
-  hash[alphabet[i]] = i if "аоиеёэыуюя".include?(alphabet[i])
+  hash[alphabet[i]] = i+1 if "аоиеёэыуюя".include?(alphabet[i])
 end
 
 puts hash
