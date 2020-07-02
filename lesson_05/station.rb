11b4408d282c70db4006e74f2cab13ae8f60c09e
Evgeni Rovneiko <erovneiko@mail.ru>
2020-07-02 17:58:03 +0300

    задание "интерфейсы, наследование, полиморфизм"

---
+++ lesson_05/station.rb
@@ -0,0 +1,23 @@
+class Station
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @trains = []
+  end
+
+  # Прибытие
+  def arrive(train)
+    @trains << train
+  end
+
+  # Отправление
+  def dispatch(train)
+    @trains.delete(train)
+  end
+
+  def trains(type = nil)
+    return @trains unless type
+    @trains.each { |t| t.type == type }    
+  end
+end
